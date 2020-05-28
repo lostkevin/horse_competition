661c9f1572c9a7202ebd1acc4f5a83a8eee37e2c
凯文 胡 <lostkevin666@gmail.com>
2020-05-28 22:53:42 +0800

    完成基础BUFF调用, 增加人物icon项,删去不必要的代码,完善了数据文件的注释

--- skills.py
+++ skills.py
@@ -1,17 +1,19 @@
 import random
 import buffs
 import copy
+
 class Skill:
     def __init__(self, owner, skill_content):
         self.skill_name = skill_content['Name']
         self.description = skill_content['Description']
         self.prob = skill_content['Prob']
-        self.action_seq = self.make_action_seq(skill_content['ActionSequence'])
+        self.action = self.make_action_seq(skill_content['ActionSequence'])
 
 
     def effect(self, owner, players):
         for action in self.action_seq:
             action(owner, players)
+        self.actions = list(filter(lambda x: hasattr(x, 'persist_turn') and x.persist_turn > 0, self.buffs))
 
     @staticmethod
     def make_action_seq(action_seq):
@@ -27,57 +29,45 @@ class Skill:
             elif target_configs[0] == 'RANK':
                 targets.sort(key=lambda x: x.pos, reversed=True)
                 return [targets[i - 1] for i in target_configs[1:] if i <= len(targets)]
+            elif target_configs[0] == 'SELF':
+                return [owner]
         actions = []
         # 对于施术对象, 有以下格式
+        # Configs['Target'] = ['SELF'], 对自身使用
         # Configs['Target'] = ['ALL'], 对除自身外所有人使用
         # Configs['Target'] = ['RANDOM', k], 对除自身外随机K人使用
         # Configs['Target'] = ['ID', 1, 2, 5] 指定赛道, 对第1, 2, 5号使用, 若参赛选手不足会忽略超出的编号
         # Configs['Target'] = ['RANK', 1, 2, 5] 指定名次, 对某几名使用
+        # 位移类技能 提供位移 Configs['Offset'] = (minspeed, maxspeed) 指定位移范围
+        # Configs['Offset'] = offset 固定位移
         for action in action_seq:
             def func(owner, players):
                 for target in choice_recipient(owner, players, action['Configs']['Target']):
-                    if action['Type'] == 'position':
-                        pass
+                    if action['Type'] == 'move':
+                        if type(action['Configs']['Offset']) is tuple:
+                            target.move(random.randint(*action['Configs']['Offset']))
+                        else: target.move(action['Configs']['Offset'])
                     elif action['Type'] == 'buff':
-                        pass
+                        # 支持的buff有: 睡眠, 禁锢(速度为0), 加减速(速度乘以固定倍率)
+                        # 睡眠: Configs['BuffDetail'] = ('Sleep', 5) 睡眠5回合
+                        # 禁锢: Configs['BuffDetail'] = ('Imprison', 5) 禁锢5回合
+                        # 加减速: Configs['BuffDetail'] = ('Speed', 5, k) 速度设置为k, 持续5回合, 为0时相当于禁锢
+                        func.persist_turn = action['Configs'][1]
+                        target.Sleep = True
+                        target.Speed = 1
+                        if action['Configs']['BuffDetail'][0] == 'Sleep':
+                            if func.persist_turn > 0:
+                                target.Sleep = True
+                                func.persist_turn -= 1
+                        elif action['Configs']['BuffDetail'][0] == 'Imprison':
+                            if func.persist_turn > 0:
+                                target.Speed = 0
+                                func.persist_turn -= 1
+                        elif action['Configs']['BuffDetail'][0] == 'Speed':
+                            if func.persist_turn == -1:
+                                target.Speed = action['Configs'][2]
+                            elif func.persist_turn > 0:
+                                target.Speed = action['Configs'][2]
+                                func.persist_turn -= 1
             actions.append(func)
         return actions
-
-
-# class RUSH(SKILL):
-#     def __init__(self,name="",describe= "",step = 1):
-#         super().__init__(name,describe)
-#         self.step = step
-
-#     def effect(self,owner,others):
-#         owner.go_ahead(self.step)
-
-# class PUT_ALL_BACK(SKILL):
-#     def __init__(self,name="",describe= "",step = 1):
-#         super().__init__(name,describe)
-#         self.step = step
-
-#     def effect(self,owner,others):
-#         for c in others:
-#             c.go_back(self.step)
-
-# class PUT_SOMEONE_BACK(SKILL):
-#     def __init__(self,name="",describe= "",step = 1,select = 'random'):
-#         super().__init__(name,describe)
-#         self.step = step
-#         self.select = select
-
-#     def effect(self,owner,others):
-#         if self.select == 'random':
-#             c = random.choice(others)
-#         else:
-#             c = get_first(others)
-#         c.go_back(self.step)
-
-# class SET_SLEEP(SKILL):
-#     def __init__(self,name="",describe= "",persist_turn=1):
-#         super().__init__(name,describe)
-#         self.persist_turn = persist_turn
-#     def effect(self,owner,others):
-#         for c in others:
-#             c.add_buff(buffs.BUFF_SLEEP(persist_turn=self.persist_turn))
